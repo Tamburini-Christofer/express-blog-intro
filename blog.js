88aec73b127268b61013124cd97e1c0c8106e9d7
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-15 14:55:56 +0200

    Creazione array  a parte + importazione + nuova rotta

--- blog.js
+++ blog.js
@@ -2,7 +2,7 @@ import { triggerAsyncId } from "async_hooks";
 import chalk from "chalk";
 
 //todo Creazione di una variabile che possa recuperare express
-const express = require('express');
+const express = require("express");
 
 //todo Creiamo una variabile a cui assegnare express
 const server = express();
@@ -11,47 +11,15 @@ const server = express();
 const PORT = 3000;
 
 //todo Impostiamo la prima rotta, quella index
-server.get('/', (req, res) => {
-    res.send(`<h1> Server del mio blog </h1>`);
+server.get("/", (req, res) => {
+  res.send(`<h1> Server del mio blog </h1>`);
 });
 
-//todo Creiamo una seconda rotta, quella dei post
-server.get('/posts', (req, res) => {
-    [
-        {
-            id: 1,
-            title: "Il ciambellone",
-            content: "",
-            image: "./images/ciambellone.jpeg",
-            tags: [],
-        }, 
-           {
-            id: 2,
-            title: "I cracker alla barbabietola",
-            content: "",
-            image: "./images/cracker_barbabietola.jpeg.jpeg",
-            tags: [],
-        }, 
-           {
-            id: 3,
-            title: "Il buonissimo pane fritto dolce",
-            content: "",
-            image: "./images/pane_fritto_dolce.jpeg",
-            tags: [],
-        }, 
-           {
-            id: 4,
-            title: "La pasta alla barbabietola",
-            content: "",
-            image: "./images/pasta_barbabietola.jpeg",
-            tags: [],
-        }, 
-           {
-            id: 5,
-            title: "La torta paesana",
-            content: "",
-            image: "./images/torta_paesana.jpeg",
-            tags: [],
-        }, 
-    ]
+//todo Importo l'array che l'ho messo in un altro file e l'ho esportato come modulo
+const posts = require("./posts");
+
+//todo Creo la rotta bacheca
+server.get("/bacheca", (req, res) => {
+    res.json({posts});
 });
+
