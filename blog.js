80395d85f5ace4c14cea8ebaa4a019daa8387d38
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-15 14:46:47 +0200

    Creazione array 5 post

--- blog.js
+++ blog.js
@@ -1,3 +1,6 @@
+import { triggerAsyncId } from "async_hooks";
+import chalk from "chalk";
+
 //todo Creazione di una variabile che possa recuperare express
 const express = require('express');
 
@@ -9,5 +12,46 @@ const PORT = 3000;
 
 //todo Impostiamo la prima rotta, quella index
 server.get('/', (req, res) => {
-    res.send("Benvenuto nel mio blog!");
+    res.send(`<h1> Server del mio blog </h1>`);
+});
+
+//todo Creiamo una seconda rotta, quella dei post
+server.get('/posts', (req, res) => {
+    [
+        {
+            id: 1,
+            title: "Il ciambellone",
+            content: "",
+            image: "./images/ciambellone.jpeg",
+            tags: [],
+        }, 
+           {
+            id: 2,
+            title: "I cracker alla barbabietola",
+            content: "",
+            image: "./images/cracker_barbabietola.jpeg.jpeg",
+            tags: [],
+        }, 
+           {
+            id: 3,
+            title: "Il buonissimo pane fritto dolce",
+            content: "",
+            image: "./images/pane_fritto_dolce.jpeg",
+            tags: [],
+        }, 
+           {
+            id: 4,
+            title: "La pasta alla barbabietola",
+            content: "",
+            image: "./images/pasta_barbabietola.jpeg",
+            tags: [],
+        }, 
+           {
+            id: 5,
+            title: "La torta paesana",
+            content: "",
+            image: "./images/torta_paesana.jpeg",
+            tags: [],
+        }, 
+    ]
 });
